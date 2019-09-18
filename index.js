bcdef3960aacbb0ddd9ac32da8033b45ec505120
linlichun <linlichun@meicai.cn>
2019-09-18 10:54:01 +0800

    减少代码运行

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ var mock = require('./mock/index');
 var server = http.createServer(function (request, response) {
   var url = request.url;
   var mockData = mock();
+  var check = true;
   if (url === '/') {
+    check = false;
     response.writeHead(200, { 'Content-Type': 'text/html' });
     fs.readFile('./index.html', 'utf-8', function (err, data) {
       if (err) {
@@ -14,26 +16,29 @@ var server = http.createServer(function (request, response) {
       response.end(data);
     })
   }
-  
-  mockData.forEach(key => {
-    if (url === key.url) {
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      response.end(JSON.stringify(key.data));
-    }
-  });
-
-  https.get(require('./config.json').host+url, (res) => {
-    var html = ""
-    res.on("data", (data) => {
-      html += data
-    })
-    res.on("end", () => {
-      response.writeHead(200, { 'Content-Type': 'application/json' });
-      response.end(html);
-    })
-  }).on('error', (e) => {
-    console.log(`Got error: ${e.message}`);
-  });
+  if (process.env.MOCK === 'open') {
+    (mockData || []).forEach(key => {
+      if (url === key.url) {
+        check = false;
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(JSON.stringify(key.data));
+      }
+    });
+  }
+  if (check) {
+    https.get(require('./config.json').host + url, (res) => {
+      var html = ""
+      res.on("data", (data) => {
+        html += data
+      })
+      res.on("end", () => {
+        response.writeHead(200, { 'Content-Type': 'application/json' });
+        response.end(html);
+      })
+    }).on('error', (e) => {
+      console.log(`Got error: ${e.message}`);
+    });
+  }
 });
 console.log('App runing at:');
 console.log('- local: http://127.0.0.1:8080');
